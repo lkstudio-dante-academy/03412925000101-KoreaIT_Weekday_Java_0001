96cd94666963f6a9c5e3d3a16e9ea1cbc86aeb45
lkstudio.dante <lkstudio.dante@gmail.com>
2025-06-30 12:16:16 +0900

    no message

---
+++ Programming/Java/Example/src/Training/Training_02/CT01Training_02.java
@@ -0,0 +1,28 @@
+package Training.Training_02;
+
+/*
+ * Java 연습 문제 2
+ * - 학점 계산 프로그램 제작하기
+ * - 점수를 입력 받아 점수에 해당하는 학점 출력하기 (+ 단, F 는 세부 학점 출력 X)
+ *
+ * 세부 학점 범위
+ * - 0 ~ 3 : -
+ * - 4 ~ 6 : 0
+ * - 7 ~ 9 : +
+ *
+ * Ex)
+ * 점수 입력 : 95
+ * A0 학점입니다.
+ */
+
+import java.util.Scanner;
+
+/**
+ * Training 2
+ */
+public class CT01Training_02 {
+	/** 초기화 */
+	public static void start(String[] args) {
+		// Do Something
+	}
+}
---
+++ Programming/Java/Example/src/Training/Training_05/CT01Training_05.java
@@ -0,0 +1,28 @@
+package Training.Training_05;
+
+/*
+ * Java 연습 문제 5
+ * - 합계 및 평균 출력하기
+ * - 사용자로부터 정수를 입력 받는다
+ * - 입력 받은 정수가 0 보다 클 경우 값을 누적 후 다시 정수를 입력 받는다
+ * - 입력 받은 정수가 0 이하 일 경우 입력을 종료하고 합계와 평균을 출력한다
+ *
+ * Ex)
+ * 1 번째 정수 입력 : 1
+ * 2 번째 정수 입력 : 3
+ * 3 번째 정수 입력 : 5
+ * 4 번째 정수 입력 : 0
+ *
+ * 합계 : 14
+ * 평균 : 3.0
+ */
+
+/**
+ * Training 5
+ */
+public class CT01Training_05 {
+	/** 초기화 */
+	public static void start(String[] args) {
+		// Do Something
+	}
+}
